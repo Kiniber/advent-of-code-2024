ed46d1b4bfa60031a14184408d994201d74ab60e
Bernhard Kinast <kiniber88@gmail.com>
2024-12-03 22:30:53 +0100

    Solved Challenge day01

---
+++ day01/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    let my_str = include_str!("../resources/input.csv");
+    println!("We are attempting the challenge for day 1");
+    println!("So here is our input list: \n{my_str}");
+    let mut list1 = vec![];
+    let mut list2 = vec![];
+    my_str.split("\n").for_each(|line| {
+            let mut next_part_iterator = line.split_whitespace();
+            if let Some(first_part_str) = next_part_iterator.next() {
+                let first_number: i32 = first_part_str.parse::<i32>().expect("Unable to parse the number");
+                list1.push(first_number);
+            } else {
+                panic!("What? why? Is the input corrupt?")
+            }
+            if let Some(second_part_str) = next_part_iterator.next() {
+                let second_number: i32 = second_part_str.parse::<i32>().expect("Unable to parse the number");
+                list2.push(second_number);
+            } else {
+                panic!("What? why? Is the input corrupt?")
+            }
+        }
+    );
+    list1.sort();
+    list2.sort();
+    //println!("Here is the list1: \n{:?}",list1);
+    //println!("Here is the list2: \n{:?}",list2);
+    let mut iter2 = list2.iter();
+    let result_list: Vec<i32> = list1.iter().map(|number1|(number1 - iter2.next().unwrap()).abs()).collect();
+    println!("Here is the result: \n\n{:?}",result_list);
+    let total = result_list.iter().sum::<i32>();
+    println!("Here is the total distance: {total}");
+}
